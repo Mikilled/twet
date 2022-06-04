ac82d81dba64de92cc536d4fe065a11d6f8fef4e
Miki <104788529+Mikilled@users.noreply.github.com>
2022-06-05 06:28:38 +0700

    Add files via upload
    added a check whether it is possible to write on twitter

--- pars.py
+++ pars.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium_stealth import stealth
+import pickle
 import time
 
 
@@ -24,8 +25,7 @@ def search_twit():
         global twiters
         for link in links:
                 driver.get(link)
-                a = driver.find_elements_by_xpath(
-                        '//*[@id="main"]/div/div/div[3]/div/div/div[2]/div/div/div[1]/div/div[1]/a')
+                a = driver.find_elements_by_xpath('//*[@id="main"]/div/div/div[3]/div/div/div[2]/div/div/div[1]/div/div[1]/a')
                 if len(a) != 0:
                         for i in a:
                                 b = i.get_attribute('href')
@@ -33,14 +33,42 @@ def search_twit():
                                         print(b)
                                         twiters.append(b)
 
+
+
+
+
+def twiter():
+        global globtwiters
+        globtwiters = list(set(globtwiters))
+        global twiters
+        global freetwit
+        for twit in globtwiters:
+                driver.get(twit)
+                time.sleep(2)
+                tw = driver.find_elements_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/div[2]/div[2]/div/span')
+                if len(tw) != 0:
+                        print('found')
+                        freetwit.append(twit)
+                else:
+                        print('not found')
+                        continue
+
+
+
+
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
 
 # options.add_argument("--headless")
 
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.managed_default_content_settings.images": 2}
+chrome_options.add_experimental_option("prefs", prefs)
+
+
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
-driver = webdriver.Chrome(options=options, executable_path=r"C:\chromedriver.exe")
+driver = webdriver.Chrome(chrome_options=chrome_options, options=options, executable_path=r"C:\chromedriver.exe")
 
 stealth(driver,
         languages=["en-US", "en"],
@@ -61,6 +89,7 @@ n = int(input('введите кол-во прогонов по opensea\n'))
 save = input('введите путь куда сохранить твитеры(без ковычек)\n')
 c = 0
 globtwiters = []
+freetwit = []
 while c != n:
         links = []
         twiters = []
@@ -69,42 +98,24 @@ while c != n:
         globtwiters = globtwiters + twiters
         c+=1
 
+
+driver.get("https://twitter.com")
+time.sleep(1)
+for cookie in pickle.load(open("C:\wit\cookies.pkl", "rb")):
+    driver.add_cookie(cookie)
+time.sleep(2)
+
+twiter()
+
+print(freetwit)
+j = ''
 f = open(save, 'w',encoding="utf-8")
-for i in range(len(globtwiters)):
-        с = globtwiters[i]
-        f.write(c+'\n')
+for i in range(len(freetwit)):
+        j = freetwit[i]
+        f.write(j+'\n')
 f.close()
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# driver.get('https://opensea.io/pashanimgotbag')
-# time.sleep(2)
-# a = driver.find_elements_by_xpath('//*[@id="main"]/div/div/div[3]/div/div/div[2]/div/div/div[1]/div/div[1]/a')
-# if len(a) != 0:
-#         for i in a:
-#                 b = i.get_attribute('href')
-#                 if 'twit' in b:
-#                         print(b)
-#
-# # for i in all_block:
-# #         print(i)
-# #         print(i.get_attribute('href'))
 
